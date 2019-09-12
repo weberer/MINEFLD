27c621cdc857a277f460c4d705da62a679ab3459
Eric <weberer@outlook.com>
2019-09-11 22:19:46 -0500

    Updates to draw_posn to correct scaling factors Updates to writeAt to allow numbers to be processed Updated constant names Fixed rows/columns constant names being swapped Updated functions to use constants instead of magic #s Fixed neighbors function Removed debug log statements

--- MINEFLD.js
+++ MINEFLD.js
@@ -1,23 +1,19 @@
-let ch,
-    done,
-    field,
-    guess,
-    mine_ct,
-    none,
-    score;
-
-// game constants
-const _screenWidth = 80; //DOS CGA screen width in characters
-const _screenHeight = 25; //DOS CGA screen height in characters
-const _linePrefix = "display_line_";
-const _enterKeyCode = "Enter";
-const _numpadEnterKeyCode = "NumpadEnter";
-const _backspaceKeyCode = "Backspace";
+//game constants
+const _screen_columns = 80; //DOS CGA screen width in characters
+const _screen_rows = 25; //DOS CGA screen height in characters
+const _mine_marker = "*";
+const _empty_marker = "█";
+const _line_prefix = "display_line_";
+const _enter_key = "Enter";
+const _numpad_enter_key = "NumpadEnter";
+const _backspace_key = "Backspace";
 const _ascii_a = 64;
-const _renderElement = "content";
-const _num_rows = 15;
-const _num_columns = 10;
+const _ascii_asterisk = _mine_marker.charCodeAt(0);
+const _parent_element = "content";
+const _num_columns = 15;
+const _num_rows = 10;
 const _input_x_location = 65;
+const _input_screen_row = 10;
 
 // display rendering characters
 const char_t = "┬"; //C2
@@ -39,13 +35,21 @@ const cube_center = char_center + char_dash + char_dash + char_dash;
 const cube_bottom_left = char_bottom_left + char_bottom + char_bottom + char_bottom;
 const cube_bottom_center = char_bottom_center + char_bottom + char_bottom + char_bottom;
 
-let displayNextWriteLine = 0;
+// Globals
+let ch,
+    displayNextWriteLine = 0,
+    done,
+    field,
+    guess,
+    mine_ct,
+    none,
+    score;
 
-function createDisplay() { //Tested
-    const parent = document.getElementById(_renderElement);
+function createDisplay() {
+    const parent = document.getElementById(_parent_element);
     let html = "";
-    for(let i = 0; i < _screenHeight; i++)
-        html += "<div id='" + _linePrefix + i + "'></div>";
+    for(let i = 0; i < _screen_rows; i++)
+        html += "<div id='" + _line_prefix + i + "'></div>";
 
     parent.innerHTML = html;
 }
@@ -63,12 +67,11 @@ function getCurrentLine() {
     return displayNextWriteLine;
 }
 
-function clrscr() { //Tested
+function clrscr() {
     // start clearing screen at top
     gotoLine(0);
-
     // clear screen by setting line to empty string
-    for(let i = 0; i < _screenHeight; i++)
+    for(let i = 0; i < _screen_rows; i++)
         writeln("");
 }
 
@@ -76,8 +79,8 @@ function writeln(content, lineNum) { //Tested
     if(lineNum) {
         if (typeof lineNum !== "number")
             throw "lineNum parameter must be of type number";
-        else if (lineNum >= _screenHeight)
-            throw "lineNum must be less than " + (_screenHeight - 1);
+        else if (lineNum >= _screen_rows)
+            throw "lineNum must be less than " + (_screen_rows - 1);
         else
             gotoLine(lineNum);
     }
@@ -85,22 +88,24 @@ function writeln(content, lineNum) { //Tested
         content = " ";
 
     // write content to screen, and move to next line
-    let element = document.getElementById(_linePrefix + displayNextWriteLine++);
-    element.innerHTML = content.padEnd(_screenWidth);
+    let element = document.getElementById(_line_prefix + displayNextWriteLine++);
+    element.innerHTML = content.padEnd(_screen_columns);
 
     // if next line is greater than max screen lines, goto first screen line.
-    if(getCurrentLine() >= _screenHeight)
+    if(getCurrentLine() >= _screen_rows)
         gotoLine (0);
 }
 
 function writeAt(xPos, yPos, text) {
-    let previousLine = getCurrentLine(); //TODO: Remove, shouldn't be needed. Kept just in case
     gotoLine(yPos);
-    let line = document.getElementById(_linePrefix + yPos);
+    let line = document.getElementById(_line_prefix + yPos);
     let content = line.innerHTML; // Get text of specified line
 
     // Break content into '3' parts, 1. Port before the new text 2. Part replaced by new text (not saved)
     // 3. Part after new text
+    if(typeof text === "number")
+        text = text.toString();
+
     let contentBefore = content.substr(0, xPos);
     let contentAfter = content.substr(xPos +text.length);
 
@@ -114,14 +119,13 @@ function getKey(callback) {
         document.removeEventListener("keydown", keydownCallback, false);
         callback(e);
     };
-
     document.addEventListener("keydown", keydownCallback , false);
 }
 
 function readAt(xPos, yPos)
 {
     // Get text from line @ yPos
-    let line = document.getElementById(_linePrefix + yPos);
+    let line = document.getElementById(_line_prefix + yPos);
     let content = line.innerHTML;
     let contentBefore = content.substr(0, xPos);
 
@@ -178,37 +182,39 @@ function isAlphaNumeric(keyCode) {
         return false;
 }
 
-var neighbors = function() {
-    let ii, jj, ct;
+/*
+    Calculates the number of mines neighboring a square.
+    A neighbor is defined as any square in the cardinal directions and diagonals
+*/
+function neighbors() {
+    let neighborY,
+        neighborX,
+        neighborCount;
 
-    for (var pp = 1; pp <= 10; pp++) {
-        for (var qq = 1; qq <= 15; qq++) {
-            if (field[pp][qq] !== 15) {
-                ii = pp - 1;
-                ct = 0;
-                while (ii != (PP + 2)) {
-                    if (ii > 0 && ii < 11) {
-                        jj = qq - 1;
-                        while (jj != (qq + 2)) {
-                            if (jj > 0 &&
-                                jj < 16 &&
-                                field[ii][jj] === 15
-                            ) {
-                                ct++;
-                            }
-                            jj++;
+    // loop through every square
+    for (let targetY = 0; targetY < _num_rows; targetY++)
+        for (let targetX = 0; targetX < _num_columns; targetX++)
+            if (field[targetY][targetX] !== _mine_marker) {
+                neighborY = targetY - 1; // Start checking one square above the target
+                neighborCount = 0;
+                while (neighborY !== (targetY + 2)) { // Stop when more than one square below the target
+                    if (neighborY > -1 && neighborY < _num_rows) { // Make sure Y coordinate is valid
+                        neighborX = targetX - 1; // Start checking one square to the left of the target
+                        while (neighborX !== (targetX + 2)) { // Stop when more than one square to the right of the target
+                            if (neighborX > -1 && neighborX < _num_columns && field[neighborY][neighborX] === _mine_marker) // If X coordinate is valid and the square contains a mine
+                                neighborCount++;
+                            neighborX++; // Check next horizontal neighbor
                         }
-                        if (ct === 0) {
-                            field[pp][qq] = '�';
-                        } else {
-                            field[pp][qq] = (48 + ct);
-                        }
+                        // Set empty marker if no neighbors, else number of neighbors
+                        if (neighborCount === 0)
+                            field[targetY][targetX] = _empty_marker;
+                        else
+                            field[targetY][targetX] = neighborCount
                     }
+                    neighborY ++; // Check next vertical neighbor
                 }
             }
-        }
-    }
-};
+}
 
 function drawBoard() {
     clrscr();
@@ -216,7 +222,7 @@ function drawBoard() {
     writeln("    1   2   3   4   5   6   7   8   9  10  11  12  13  14  15");
     writeln("  " + cube_top_left + cube_top + cube_top + cube_top + cube_top + cube_top + cube_top + cube_top + cube_top + cube_top + cube_top + cube_top + cube_top + cube_top + cube_top + cube_top_right);
 
-    for (let i = 1; i <= _num_columns; i++) {
+    for (let i = 1; i <= _num_rows; i++) {
         writeln(String.fromCharCode(_ascii_a + i) + " │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │");
         writeln("  " + cube_left + cube_center + cube_center + cube_center+ cube_center+ cube_center+ cube_center+ cube_center+ cube_center+ cube_center+ cube_center+ cube_center+ cube_center+ cube_center+ cube_center + char_right);
     }
@@ -225,44 +231,48 @@ function drawBoard() {
     writeln("  " + cube_bottom_left + cube_bottom_center + cube_bottom_center + cube_bottom_center + cube_bottom_center + cube_bottom_center + cube_bottom_center + cube_bottom_center + cube_bottom_center + cube_bottom_center + cube_bottom_center + cube_bottom_center + cube_bottom_center + cube_bottom_center + cube_bottom_center  + char_bottom_right);
 
     draw_posn();
+    neighbors();
     // Draw input prompt and cursor
-    writeAt(_input_x_location, 9, "Move (eg A10X)?");
-    writeAt(_input_x_location, 10, "_");
+    writeAt(_input_x_location, (_input_screen_row - 1), "Move (eg A10X)?");
+    writeAt(_input_x_location, _input_screen_row, "_");
 }
 
 var draw_posn = function() {
     // Offsets to position characters in the center of grids
     const _x_offset = 4,
-          _y_offset = 3;
+          _x_scale = 3,
+          _y_offset = 3,
+          _y_scale = 2;
 
-    for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < 15; j++) {
-            if (guess[i][j] !== 0) {
-                writeAt((j* 4 + _x_offset), (i * 2 + _y_offset), field[i][j]); //TODO: Add ability to color text
+
+    for (let y = 0; y < _num_rows; y++) {
+        for (let x = 0; x < _num_columns; x++) {
+            if (guess[y][x] !== 0) {
+                writeAt((x + (x * _x_scale) + _x_offset), (y * _y_scale + _y_offset), field[y][x]); //TODO: Add ability to color text
             }
         }
     }
 };
 
 function seed_mines() {
-    for (let i = 0; i < _num_columns; i++) {
-        for (let j = 0; j < _num_rows; j++) {
+    for (let i = 0; i < _num_rows; i++) {
+        for (let j = 0; j < _num_columns; j++) {
             field[i][j] = " ";
         }
     }
 
-    for (let i = 0; i < _num_columns; i++) {
-        for (let j = 0; j < _num_rows; j++) {
+    for (let i = 0; i < _num_rows; i++) {
+        for (let j = 0; j < _num_columns; j++) {
             guess[i][j] = 0;
         }
     }
 
     for (let i = 0; i < mine_ct; i++) {
-        let x = getRandom(0, _num_columns),
-            y = getRandom(0, _num_rows);
+        let x = getRandom(0, _num_rows),
+            y = getRandom(0, _num_columns);
         while (field[x][y] !== ' ') {
-            x = getRandom(0, _num_columns);
-            y = getRandom(0, _num_rows);
+            x = getRandom(0, _num_rows);
+            y = getRandom(0, _num_columns);
         }
         field[x][y] = "*";
     }
@@ -301,16 +311,16 @@ var check_empty = function(du, rl) {
 };
 
 function show_board() {
-    for (let i = 0; i < _num_columns; i++)
-        for (let j = 0; j < _num_rows; j++)
+    for (let i = 0; i < _num_rows; i++)
+        for (let j = 0; j < _num_columns; j++)
             if (guess[i][j] === 0)
                 guess[i][j] = 15;
 }
 
-var get_guess = function() {
+function get_guess() {
     let startingWritePos = _input_x_location;
     let getKeyCallback = function(e) {
-        if(e.code === _enterKeyCode || e.code === _numpadEnterKeyCode)
+        if(e.code === _enter_key || e.code === _numpad_enter_key)
             submit();
         else
         {
@@ -319,30 +329,30 @@ var get_guess = function() {
             if(e.key.length === 1)
                 alphaNumeric = isAlphaNumeric(e.key.charCodeAt(0));
 
-            if(e.code === _backspaceKeyCode) // backspace
+            if(e.code === _backspace_key) // backspace
             {
                 // On backspace press, remove previous character if there are characters present
                 if(startingWritePos > _input_x_location) {
-                    writeAt(startingWritePos--, 10, " ");
-                    writeAt(startingWritePos, 10, "_");
+                    writeAt(startingWritePos--, _input_screen_row, " ");
+                    writeAt(startingWritePos, _input_screen_row, "_");
                 }
             }
-            else if(alphaNumeric) // 0-9, A-Z
+            else if(alphaNumeric) // 0-9, A-Z, ?
             {
-                writeAt(startingWritePos++, 10, e.key);
-                writeAt(startingWritePos, 10, "_");
+                writeAt(startingWritePos++, _input_screen_row, e.key);
+                writeAt(startingWritePos, _input_screen_row, "_");
             }
             getKey(getKeyCallback);
         }
     };
 
-function submit() {
+    var submit = () => {
         let ok,
             row,
             column,
             flag = " ";
 
-        let choice = readAt(_input_x_location, 10);
+        let choice = readAt(_input_x_location, _input_screen_row);
         // get first character of choice
         let ch = choice.charAt(0).toUpperCase();
 
@@ -359,12 +369,12 @@ function submit() {
             column = 100;
         else column--;
 
-        if(ch.charCodeAt(0) > 64)
-            row = ch.charCodeAt(0) - 65;
+        if(ch.charCodeAt(0) > _ascii_a)
+            row = ch.charCodeAt(0) - _ascii_a - 1; //Subtract 1 more than a to align with array 0 index
         else
             row = 100;
 
-        ok = (flag === " " || flag === "?" || flag === "M") && guess[row][column] === 0 && row >= 0 && row < 11 && column >= 0 && column < 16;
+        ok = (flag === " " || flag === "?" || flag === "M") && guess[row][column] === 0 && row > -1 && row <= _num_rows && column > -1 && column <= _num_columns;
         if(!ok)
         {
             //Play a sound
@@ -377,9 +387,10 @@ function submit() {
 
             switch (flag) {
                 case " ":
-                    if (field[row][column] === String.fromCharCode(15)) {
+                    if (field[row][column] === String.fromCharCode(_ascii_asterisk)) {
                         done = true;
                         show_board();
+                        drawBoard();
                     } else
                         check_empty(row, column);
                     break;
@@ -389,7 +400,7 @@ function submit() {
                     break;
 
                 case "M":
-                    if (field[row][column] !== String.fromCharCode(15)) {
+                    if (field[row][column] !== String.fromCharCode(_ascii_asterisk)) {
                         done = true;
                         show_board();
                     } else {
@@ -398,16 +409,18 @@ function submit() {
                     }
                     break;
             }
-            console.log(guess); //TODO: Remove Me
 
             if (!done) {
                 done = (score === 150);
+                drawBoard();
                 get_guess();
             }
         }
     };
+
+    // Begin execution of get_guess function
     getKey(getKeyCallback);
-};
+}
 
 var run = function() {
     score = 0;
@@ -476,7 +489,7 @@ function getRandom(min, max) {
 document.addEventListener("DOMContentLoaded", function(event)
 {
     let getKeyCallback = function(e) {
-        if(e.code === _enterKeyCode || e.code === _numpadEnterKeyCode)
+        if(e.code === _enter_key || e.code === _numpad_enter_key)
             run();
         else
             getKey(getKeyCallback);
